f7d0889ab4647fe2570f2e55074eb9fcf00d6fe0
Richard Santos <rsantos@tray.com.br>
2016-08-18 13:30:00 -0300

    Components + routes

---
+++ js/config/routes.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
+
+// Layouts
+import App from './layout/app.jsx';
+
+// Pages
+import Home from './pages/home.jsx';
+import Items from './pages/items.jsx';
+
+// Routes
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="items" component={Items} />
+    </Route>
+  </Router>
+), document.getElementById('app'))
---
+++ js/pages/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Breadcrumb from '../components/breadcrumb.jsx';
+
+class Home extends React.Component {
+  render () {
+    return (
+        <div>
+            <Breadcrumb />
+        </div>
+    );
+  }
+}
+
+export default Home;
