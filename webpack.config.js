3bd8f52be3495362854114b40262a5622cd1d721
Richard Santos <rsantos@tray.com.br>
2016-08-23 16:39:02 -0300

    Code style

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,23 @@
-var webpack = require('webpack');
 var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'js/dist');
 var APP_DIR = path.resolve(__dirname, 'js');
 
 var config = {
-  entry: APP_DIR + '/main.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel'
-      }
-    ]
-  }
+    entry: APP_DIR + '/main.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?/,
+                include: APP_DIR,
+                loader: 'babel'
+            }
+        ]
+    }
 };
 
 module.exports = config;
