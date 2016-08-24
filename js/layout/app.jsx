2fa0ec17c23005ee1f3fbd7f0ecd2232ad580115
Richard Santos <rsantos@tray.com.br>
2016-08-24 16:53:57 -0300

    Added: active routes in sidebar

--- js/layout/app.jsx
+++ js/layout/app.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 
 import Header from '../components/Header.jsx';
 import Sidebar from '../components/Sidebar.jsx';
 
 class App extends React.Component {
-  render () {
-    return (
-      <div>
-        <Header />
-        <Sidebar />
-        <main className="ls-main">
-          <div className="container-fluid">
-            {this.props.children}
-          </div>
-        </main>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Header />
+                <Sidebar router={this.props.router} />
+                <main className="ls-main">
+                    <div className="container-fluid">
+                        {this.props.children}
+                    </div>
+                </main>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default withRouter(App);
