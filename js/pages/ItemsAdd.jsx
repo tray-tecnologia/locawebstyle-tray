5abf9eecbce402b99312d7f2de147f8d85ef57c9
Richard Santos <rsantos@tray.com.br>
2016-08-26 18:21:38 -0300

    Added: Collapse for Tray users

--- js/pages/ItemsAdd.jsx
+++ js/pages/ItemsAdd.jsx
@@ -24,7 +24,33 @@ class ItemsAdd extends React.Component {
         return (
             <div>
                 <Breadcrumb title="Incluir Item" />
-                <Alert message={this.state.alert.message} title={this.state.alert.title} type={this.state.alert.type} dismissable="true" />
+
+                <form action="">
+                    <div data-ls-module="collapse" data-target="#30" className="ls-collapse ls-box ls-box-gray">
+                        <a href="#" className="ls-collapse-header">
+                            <h3 className="ls-collapse-title">
+                                Exclusivo Tray
+                            </h3>
+                            <p>
+                                Itens de acesso exclusivo para funcionários Tray.
+                            </p>
+                        </a>
+                        <div className="ls-collapse-body" id="30">
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <h3>Configurações</h3>
+                                    <p>Configurações referentes a exibição do item.</p>
+                                </div>
+                                <div className="col-md-8">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <Alert message={this.state.alert.message} title={this.state.alert.title} type={this.state.alert.type} dismissable="true" />
+                </form>
+
             </div>
         );
     }
