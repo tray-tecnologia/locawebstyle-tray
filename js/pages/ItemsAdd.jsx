d972b1b5e10a49b3459d7caf2c62c0d1ed5892bb
Richard Santos <rsantos@tray.com.br>
2016-08-26 18:10:56 -0300

    Added: Alert component

--- js/pages/ItemsAdd.jsx
+++ js/pages/ItemsAdd.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 
 import Breadcrumb from '../components/Breadcrumb.jsx';
+import Alert from '../components/Alert.jsx';
 
 class ItemsAdd extends React.Component {
-  render () {
-    return (
-        <div>
-            <Breadcrumb title="Incluir Item" />
-            Adding new item...
-        </div>
-    );
-  }
+
+    constructor() {
+        super();
+        this.state = {
+            alert: {
+                message: '',
+                title: '',
+                type: '',
+            }
+        }
+    }
+
+    showAlert(options) {
+        this.setState({ alert: options });
+    }
+
+    render() {
+        return (
+            <div>
+                <Breadcrumb title="Incluir Item" />
+                <Alert message={this.state.alert.message} title={this.state.alert.title} type={this.state.alert.type} dismissable="true" />
+            </div>
+        );
+    }
 }
 
 export default ItemsAdd;
