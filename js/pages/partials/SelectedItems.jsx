4dd14899a5886e24b97ea841cf90a45a27c4e921
Richard Santos <rsantos@tray.com.br>
2016-09-02 10:16:13 -0300

    Refact: Selected items table

---
+++ js/pages/partials/SelectedItems.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default class SelectedItems extends React.Component {
+    render() {
+
+        let items = this.props.items.filter((element) => {
+            return element.selected;
+        }).map((element, index) => {
+            return (
+                <tr key={index}>
+                    <td className="ls-text-sm">{element.id} - {element.name}</td>
+                    <td className="ls-text-sm ls-txt-right">R$ 99,00</td>
+                    <td className="ls-text-sm ls-txt-right">
+                        <a href="#" aria-label="Ver imagem do produto" className="ls-text-xl ls-sm-margin-right ls-tooltip-left">
+                            <span className="ls-ico-images"></span>
+                        </a>
+                        <button className="ls-btn-xs ls-btn-danger" onClick={this.props.onRemove} data-item-id={element.id}>Remover</button>
+                    </td>
+                </tr>
+            );
+        });
+
+        if (!items.length) {
+            return null;
+        }
+
+        return (
+            <div>
+                <h5>Produtos selecionados</h5>
+                <table className="ls-table ls-table-striped ls-table-bordered ls-no-margin-bottom">
+                    <tbody>
+                        {items}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
