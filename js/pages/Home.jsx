a51ae4fba7e7a4daa82ea2206a44f2e50a877fea
Richard Santos <rsantos@tray.com.br>
2016-09-13 11:01:20 -0300

    Refact

--- js/pages/Home.jsx
+++ js/pages/Home.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 import Breadcrumb from '../components/Breadcrumb.jsx';
 
 class HomePage extends React.Component {
-  render () {
-    return (
-        <div>
-            <Breadcrumb title="Início" />
-        </div>
-    );
-  }
+
+    render() {
+        return (
+            <div>
+                <Breadcrumb title="Início" />
+            </div>
+        );
+    }
+
 }
 
 export default HomePage;
