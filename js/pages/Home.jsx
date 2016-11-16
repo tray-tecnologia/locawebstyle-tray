2624d0dd5c9f3fe7ed8361ef77b6881b6c697ac5
Richard Santos <rsantos@tray.com.br>
2016-11-16 16:26:59 -0200

    Home page

--- js/pages/Home.jsx
+++ js/pages/Home.jsx
@@ -8,6 +8,20 @@ class HomePage extends React.Component {
         return (
             <div>
                 <Breadcrumb title="Início" />
+
+                <p className="ls-md-margin-bottom">
+                    A ideia desse projeto é padronizar o uso dos componentes do LS (LocawebStyle),
+                    dando exemplos reais de implementações.<br />
+
+                    Lembre-se de alguns detalhes quando estiver trabalhando com o LS:
+                </p>
+
+                <p>
+                    - Não escreva nenhuma linha de CSS (o LS já é sua implementação de design)<br />
+                    - Utilize NPM (ou Yarn) ao invés do Bower (NPM for all things)<br />
+                    - Nem tudo é função do LS, você vai precisar de outras dependências
+                </p>
+
             </div>
         );
     }
