0d007083dcabfb1b3d6d2ff0a3dc3eaa473ad13d
Richard Santos <rsantos@tray.com.br>
2016-12-20 10:11:01 -0200

    Added Yarn

--- js/pages/Home.jsx
+++ js/pages/Home.jsx
@@ -16,11 +16,11 @@ class HomePage extends React.Component {
                     Lembre-se de alguns detalhes quando estiver trabalhando com o LS:
                 </p>
 
-                <p>
-                    - Não escreva nenhuma linha de CSS (o LS já é sua implementação de design)<br />
-                    - Utilize NPM (ou Yarn) ao invés do Bower (NPM for all things)<br />
-                    - Nem tudo é função do LS, você vai precisar de outras dependências
-                </p>
+                <ul className="ls-md-margin-left">
+                    <li>Não escreva nenhuma linha de CSS (o LS já é sua implementação de design)</li>
+                    <li>Utilize NPM (ou Yarn) ao invés do Bower (NPM for all things)</li>
+                    <li>Nem tudo é responsabilidade do LS, você vai precisar de outras dependências</li>
+                </ul>
 
             </div>
         );
